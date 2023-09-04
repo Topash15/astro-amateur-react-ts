88876229ff0b46d9d82a280dff3d699867f7908c
Alexander Topash <Alextopash@gmail.com>
2023-09-04 12:54:31 -0400

    File handling

--- src/components/PhotoEditor/index.tsx
+++ src/components/PhotoEditor/index.tsx
@@ -1,45 +1,65 @@
 import { Image } from "image-js";
 import { SetStateAction, useState } from "react";
 import handleImages from '../../utils/imageEditor';
+import axios from 'axios';
 
 function PhotoEditor() {
-  const [fileName, setFileName] = useState("");
+  const [file, setFile] = useState("");
   const [SDImage, setSDImage] = useState("");
   const [Thumbnail, setThumbnail] = useState("");
   const [Placeholder, setPlaceholder] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleChange = (e: any) => {
-    setFileName(e.target.value);
-    console.log(fileName);
+    setFile(e.target.value);
+    console.log(file);
   };
 
   const handleSubmit = async (e: any) => {
-    // alert("Submitted");
+    e.preventDefault();
 
-    setFileName('src/assets/falcon-9-jellyfish.png')
-    const files = await handleImages(fileName);
-    setSDImage(files.standard);
-    setThumbnail(files.thumbnail);
-    setPlaceholder(files.placeholder);
+    if(!file){
+      alert("No file found. Please choose a file before submitting.");
+      return;
+    }
+
+    const fd = new FormData();
+    fd.append('file', file);
+
+    axios.post('', fd, {
+      onUploadProgress: (progressEvent) => {
+        console.log(progressEvent.progress || 'loading')
+      },
+      headers: {
+        'Custom-Header' : "value"
+      }
+    })
+
+
+    // setFile('src/assets/falcon-9-jellyfish.png')
+    // const files = await handleImages(file);
+    // setSDImage(files.standard);
+    // setThumbnail(files.thumbnail);
+    // setPlaceholder(files.placeholder);
 
     e.preventDefault();
     setIsSubmitted(true);
+    alert("Submitted");
     return 
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        {/* <label>
+        <label>
           Image
           <input
             type="file"
             name="image"
-            value={fileName}
+            value={file}
             onChange={handleChange}
           />
-        </label> */}
+        </label>
         <input type="submit" value="Submit" />
       </form>
       <img src="src/assets/falcon-9-jellyfish.png" alt="test-image" width="150" />
