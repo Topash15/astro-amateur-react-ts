e54b91292f49518ae8ea00d880d0f35c1c2c9d90
Alexander Topash <Alextopash@gmail.com>
2024-01-17 13:15:05 -0500

    homepage links moved

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,36 @@
 import "./style.css"
 import { SERVER } from '../../environment'
 
-function Home(){
+function Home() {
 
-    // current server goes idle if not used
-    // this api call wakes up the server and will improve the load times
-    // for following server calls
-    const wakeServer = async () => {
-        return await (
-          await fetch(
-            `${SERVER}/api/portfolio/get/photo/1`
-          )
-        ).json();
-      };
+  // current server goes idle if not used
+  // this api call wakes up the server and will improve the load times
+  // for following server calls
+  const wakeServer = async () => {
+    return await (
+      await fetch(
+        `${SERVER}/api/portfolio/get/photo/1`
+      )
+    ).json();
+  };
 
-    return(
-        <>
-            <div className="home-container">
-                <h2>Astro Amateur</h2>
-                <p>I photograph rockets and space.
-                </p>
-                <a href="/#/photos">Check them out.</a>
-                <div id="home-filters">
-                  <div><a href="/#/photos/Moon">Moon</a><img src="./assets/home/moon.jpg" alt="Moon photos" width="200"/></div>
-                  <div><a  href="#/photos/Rocket">Rockets</a><img src="./assets/home/rocket.jpg" alt="Rocket photos" width="200"/></div>
-                  <div><a href="#/photos/Deep Space">Deep Space</a><img src="./assets/home/deep-space.jpg" alt="Deep Space photos" width="200"/></div>
-                  <div><a href="#/photos/Planet">Planets</a><img src="./assets/home/planets.png" alt="Planet photos" width="200"/></div>
-                </div>
-                
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <div className="home-container">
+        <h2>Astro Amateur</h2>
+        <p>I photograph rockets and space.
+        </p>
+        <a href="/#/photos">Check them out.</a>
+      </div>
+      <div id="home-filters">
+        <div><a href="/#/photos/Moon">Moon</a><img src="./assets/home/moon.jpg" alt="Moon photos" width="200" /></div>
+        <div><a href="#/photos/Rocket">Rockets</a><img src="./assets/home/rocket.jpg" alt="Rocket photos" width="200" /></div>
+        <div><a href="#/photos/Deep Space">Deep Space</a><img src="./assets/home/deep-space.jpg" alt="Deep Space photos" width="200" /></div>
+        <div><a href="#/photos/Planet">Planets</a><img src="./assets/home/planets.png" alt="Planet photos" width="200" /></div>
+      </div>
+
+    </>
+  )
 }
 
 export default Home;
