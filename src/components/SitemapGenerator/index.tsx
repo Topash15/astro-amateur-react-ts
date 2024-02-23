e8765c51e946695a4a973ff4794a8307730ecd88
Alexander Topash <Alextopash@gmail.com>
2024-02-23 11:41:03 -0500

    Add sitemap generator to admin page

---
+++ src/components/SitemapGenerator/index.tsx
@@ -0,0 +1,72 @@
+import { SERVER, LIVE_URL } from "../../environment";
+import { useQuery } from "react-query";
+import Photo from "../../types/Photo";
+
+function SiteMap() {
+
+    const fetchAllPhotos = async () => {
+        return await (
+            await fetch(
+                `${SERVER}/api/portfolio/get/photos`
+            )
+        ).json();
+    };
+    type Photos = {
+        results: Photo[];
+    };
+
+    const { data, error, status } = useQuery<Photos, Error>(
+        "photos",
+        fetchAllPhotos
+    );
+    const photoList: Photo[] | [] = data?.results || [{
+        id: 0,
+        title: "No Photos Found",
+        description: "There may be a connection issue. Please refresh or try later",
+        detailedDescription: '',
+        camera: '',
+        lens: '',
+        iso: 0,
+        aperture: '',
+        thumbnail: '',
+        hdSource: '',
+        source: '',
+        link: '',
+        date: '',
+        theme: '',
+        exposureTime: 0,
+    }];
+
+    const dynamicSiteMap: string = photoList.map((photo)=>{return `
+    <url>
+        <loc>https://www.astroamateur.space/#/photos/id/${photo.id}</loc>
+    </url>
+    `}).join('')
+    const siteMap: string = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset>
+    <url>
+        <loc>https://www.astroamateur.space/</loc>
+    </url>
+    <url>
+        <loc>https://www.astroamateur.space/#/photos</loc>
+    </url>
+        ${dynamicSiteMap}
+    </urlset>`
+
+    const copySitemap = () => {
+        navigator.clipboard.writeText(siteMap);
+        window.alert("Sitemap copied to clipboard");
+    }
+
+    return(
+    <>
+    <div id="sitemap-container">
+        {siteMap}
+    </div>
+    <button onClick={copySitemap}>Copy Sitemap</button>
+    </>)
+}
+
+
+
+export default SiteMap;
--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,10 +1,13 @@
 import PhotoEditor from "../../components/PhotoEditor";
+import SiteMap from "../../components/SitemapGenerator";
 
 function Admin(){
     return(
         <>
             <h2>Admin</h2>
+
             <PhotoEditor />
+            <SiteMap/>
         </>
     )
 }
